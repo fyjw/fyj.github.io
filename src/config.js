bebeeb9e4171a453f43e63bc9d6c1cc51c77427f
fyjw <songnian02@gmail.com>
2024-03-29 10:32:20 +0800

    网站

---
+++ src/config.js
@@ -0,0 +1,127 @@
+export default {
+  // page1部分
+  page1: {
+    titleEn: 'Hello,I`m FanYinjun', //英文标题
+    title: '一个追梦在医护路上的小白', //中文标题
+  },
+  // page2部分
+  page2: {
+    authorImg: 'page1.jpg', // 作者头像
+    xinhui: '我叫樊银君，是一名在读的大二学生。', // 幸会
+    qiuzhi: '松山医院超级无敌大boss', // 求职意向
+    guanyuwo: '掌握了护理的所有知识，不要问，问就是会。不要细问，因为不是太会' // 关于我
+  },
+  // page3部分
+  page3: [{
+    icon: 'icon-tubiao-',
+    title: '能吃',
+    msg: ['超级能吃']
+  },{
+    icon: 'icon-diannao', // 图标
+    title: '能睡', // 标题
+    msg: ['超级能睡'] //介绍
+  }, {
+    icon: 'icon-qianbi1',
+    title: '贪玩',
+    msg: ['超级无敌贪玩']
+  },  {
+    icon: 'icon-shouji',
+    title: '不洗澡',
+    msg: ['不爱洗澡']
+  }],
+  // page4部分
+  page4: {
+    // 我的历程
+    course: [{
+      date: '2023/9——至今', // 时间
+      desc: { // 经历
+        title: '重庆护理职业学院',
+        list: ['重庆护理职业学院超级大专家','重庆松山医院实习']
+      }
+    }, {
+      date: '2021/9——2022/9',
+      desc: {
+        title: '重庆护理职业学院',
+        list: ['教研室助理']
+      }
+    }, {
+      date: '2020/9——2021/9',
+      desc: {
+        title: '重庆护理职业学院',
+        list: ['正在修炼的无敌无敌小白']
+      }
+    }],
+    // 我的拓展技能掌握
+    singlelist: [{
+      title: '洗手',
+      text: '了解'
+    }, {
+      title: '刷牙',
+      text: '掌握'
+    }, {
+      title: '吃饭',
+      text: '掌握'
+    }, {
+      title: '睡觉',
+      text: '掌握'
+    }],
+    // 我的基本技能掌握
+    proresslist: [{
+      title: '吃冒菜',
+      value: '90%'
+    }, {
+      title: '吃麻辣香锅',
+      value: '90%'
+    }, {
+      title: '吃串串',
+      value: '90%'
+    }, {
+      title: '吃火锅',
+      value: '80%'
+    }, {
+      title: '喝奶茶',
+      value: '80%'
+    }]
+  },
+  // page5部分
+  page5: [{
+    title: '826工作室',
+    content: '重庆护理职业学院826工作室独立站点的维护',
+    image: 'box1.png',
+    href: 'https://lab508.gitee.io/'
+  }, {
+    title: '图书管理系统',
+    content: 'Vue,NodeJs图书管理系统前后端',
+    image: 'box3.jpg',
+    href: 'https://gitee.com/wttAndroid/book_admin'
+  }, {
+    title: '校园约吧',
+    content: 'Vue,NodeJs移动端校园项目前后端',
+    image: 'box5.jpg',
+    href: 'https://gitee.com/wttAndroid/xyy_server'
+  }, {
+    title: '蘑菇街',
+    content: 'Vue蘑菇街商城案例前端',
+    image: 'box2.png',
+    href: 'https://github.com/wttAndroid/MyShoppingStreet'
+  }, {
+    title: '个人简历',
+    content: 'html,css,jq,bootsrap搭建个人简历网站',
+    image: 'box4.png',
+    href: ''
+  }, {
+    title: '二维码生成器',
+    content: 'Android二维码生成器',
+    image: 'box6.jpeg',
+    href: ''
+  }],
+  // page6部分
+  page6: {
+    github: 'https://github.com/wttAndroid',
+    bili: '',
+    email: 'Mailto:1457321681@qq.com?Subject=邮箱标题&Body=邮箱内容！',
+    zhihu: '',
+    weixin: "<img style='width:120px' src='https://img1.imgtp.com/2023/07/15/QxBckw7r.jpg' alt='加载失败'>",
+    qq: "<img style='width:120px' src='https://img1.imgtp.com/2023/07/15/fGv7LpvM.jpg' alt='加载失败'>"
+  }
+}
